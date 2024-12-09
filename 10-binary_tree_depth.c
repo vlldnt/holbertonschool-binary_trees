f4e9361562514188caefc2ff07deb2f7ba97ad57
vlldnt <vieilledent.adrien@gmail.com>
2024-12-09 15:16:24 +0100

    On task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the depth of a node in a tree
+ * @tree: pointer to the node to measure depth
+ * Return: depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t)
+{
+
+}

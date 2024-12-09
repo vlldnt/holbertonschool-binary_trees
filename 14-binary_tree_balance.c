cd19227b5f306db671770fff5925d4b994051f1d
vlldnt <vieilledent.adrien@gmail.com>
2024-12-09 16:52:10 +0100

    TAsk 14 corrected with a new binary_tree_height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,21 +6,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t count_left = 0, count_right = 0;
+	int left_height = 0;
+	int right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	count_left = binary_tree_height(tree->left);
-	count_right = binary_tree_height(tree->right);
-
-	if (count_left >= count_right)
-		return (count_left + 1);
-	else
-		return (count_right + 1);
+	if (left_height > right_height)
+		return (left_height + 1);
+	return (right_height + 1);
 }
 
 /**

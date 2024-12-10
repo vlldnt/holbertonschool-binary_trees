5bf2b18ef9592b236878fa0b6d9b3e3b3a58e2d4
vlldnt <vieilledent.adrien@gmail.com>
2024-12-10 09:56:07 +0100

    task 18 find the uncle in the tree completed

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,7 +6,10 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node)
+	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	return (node->parent->parent->right);
 }

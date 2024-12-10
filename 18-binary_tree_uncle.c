5015caad282e2f3252ac88c01f7e9e384075804e
vlldnt <vieilledent.adrien@gmail.com>
2024-12-10 09:30:20 +0100

    Task 18 started

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - find the uncle node of a node
+ * @node: node where to start finding for the uncle
+ * Return: The uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+
+}
